c9fa348a3747f9926cc2119e1667fb272a736627
saifularafat <saifularafat.info@gmail.com>
2023-04-10 15:48:02 +0600

    images and content ui design compolet

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { BriefcaseIcon } from '@heroicons/react/24/solid'
+import { BriefcaseIcon, Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
     return (
-        <div>
-            <div className='my-container flex justify-between items-center bg-slate-200 py-5'>
+        <div className=' sticky top-0 z-10'>
+            <div className='my-container flex justify-between items-center bg-white shadow-xl py-5'>
+                {/* logo section */}
                 <Link to='/'>
-                    <div className='flex items-center'>
-                        <BriefcaseIcon className="h-8 w-8 text-slate-500" />
+                    <div className='relative flex items-center'>
+                        <BriefcaseIcon className="h-8 w-8 text-[#9873FF]" />
                         <p className='tracking-wide'>
                             <span className='font-serif text-3xl font-semibold text-rose-700'>D</span>
                             <span className='title  text-sky-500'>e</span>
@@ -22,7 +23,7 @@ const Header = () => {
                     </div>
                 </Link>
                 {/* nav items section activities */}
-                <ul className=' lg:flex items-center hidden space-x-8'>
+                <ul className=' md:flex items-center hidden space-x-8'>
                     <li>
                         <NavLink
                             to='/'
@@ -56,6 +57,80 @@ const Header = () => {
                 {/* star button style */}
                 <div>
                     <button className='btn-star'>Star Applying</button>
+                </div>
+
+                {/* mobile bar section */}
+                <div className='md:hidden'>
+                    {/* bars Buttons added */}
+                    <button
+                        aria-label='Open Menu'
+                        title='Open Menu'
+                        onClick={() => setIsMenuOpen(true)}
+                    >
+                        <Bars3BottomRightIcon className="h-9 w-9 text-[#9873FF]" />
+                    </button>
+                    {
+                        isMenuOpen && (
+                            <div className='absolute top-0 left-0 w-full z-10'>
+                                <div className='flex justify-between items-center p-5 bg-white shadow-2xl border'>
+                                    {/* page title section */}
+                                    <div>
+                                        <Link to='/'>
+                                            <div className=' inline-flex items-center'>
+                                                <BriefcaseIcon className="h-8 w-8 text-[#9873FF]" />
+                                                <p className='tracking-wide'>
+                                                    <span className='font-serif text-3xl font-semibold text-rose-700'>D</span>
+                                                    <span className='title  text-sky-500'>e</span>
+                                                    <span className='title text-orange-500'>v</span>
+                                                    <span className='font-serif text-3xl font-semibold text-purple-800'>Z</span>
+                                                    <span className='title text-blue-700'>o</span>
+                                                    <span className='title text-red-800'>n</span>
+                                                    <span className='title text-pink-700'>e</span>
+                                                </p>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                    {/* dropdown menu and close button */}
+                                    <div>
+                                        <button
+                                            aria-label='Close Menu'
+                                            title='Close MEnu'
+                                            onClick={() => setIsMenuOpen(false)}
+                                        >
+                                            <XMarkIcon className="w-9 text-[#5d6fe3]" />
+                                        </button>
+                                    </div>
+                                </div>
+                                {/* Mobile nav section */}
+                                <div>
+                                    <nav>
+                                        <ul className='space-y-4 bg-white h-fit'>
+                                            <li className='px-3 hover:bg-slate-200'>
+                                                <Link to='/' className='default'>
+                                                    Home
+                                                </Link>
+                                            </li>
+                                            <li className='px-3 hover:bg-slate-200'>
+                                                <Link to='statistics' className='default'>
+                                                    Statistics
+                                                </Link>
+                                            </li>
+                                            <li className='px-3 hover:bg-slate-200'>
+                                                <Link to='application' className='default'>
+                                                    Application Job
+                                                </Link>
+                                            </li>
+                                            <li className='px-3 hover:bg-slate-200'>
+                                                <Link to='blog' className='default'>
+                                                    Blog
+                                                </Link>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        )
+                    }
                 </div>
             </div>
         </div>
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import Lottie from "lottie-react";
+import Application from "../../../public/job.json";
 const Home = () => {
     return (
-        <div>
-            <p>Home Component</p>
+        <div className='md:flex justify-between items-center'>
+            <div className='w-[570px] mx-auto pl-11 border '>
+                <h2 className=' text-6xl font-bold leading-tight text-gray-800'>
+                    One Step <br /> 
+                    Closer To Your <br /> 
+                    <span className='text-[#9873FF]'>Dream Job</span>
+                </h2>
+                <p className='max-w-[500px] text-lg text-slate-500 md:mb-8 my-3 md:mt-6'>Explore thousands of job opportunities with all the <br /> information you need. Its your future. Come find it. Manage all <br /> your job application from start to finish.</p>
+
+                <button className='btn-star'>Get Started</button>
+            </div>
+            <div>
+                <Lottie animationData={Application} loop={true} />
+            </div>
         </div>
     );
 };
