47edb69dab911b9b118315e4674627cd20b14577
saifularafat <saifularafat.info@gmail.com>
2023-04-10 12:21:23 +0600

    nav ber activaties done!

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,29 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { BriefcaseIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     return (
         <div>
             <div className='my-container flex justify-between items-center bg-slate-200 py-5'>
                 <Link to='/'>
-                    <p className='tracking-wide'>
-                        <span className='font-serif text-3xl font-semibold text-rose-700'>D</span>
-                        <span className='title  text-sky-500'>e</span>
-                        <span className='title text-orange-500'>v</span>
-                        <span className='font-serif text-3xl font-semibold text-purple-800'>Z</span>
-                        <span className='title text-blue-700'>o</span>
-                        <span className='title text-red-800'>n</span>
-                        <span className='title text-pink-700'>e</span>
-                    </p>
+                    <div className='flex items-center'>
+                        <BriefcaseIcon className="h-8 w-8 text-slate-500" />
+                        <p className='tracking-wide'>
+                            <span className='font-serif text-3xl font-semibold text-rose-700'>D</span>
+                            <span className='title  text-sky-500'>e</span>
+                            <span className='title text-orange-500'>v</span>
+                            <span className='font-serif text-3xl font-semibold text-purple-800'>Z</span>
+                            <span className='title text-blue-700'>o</span>
+                            <span className='title text-red-800'>n</span>
+                            <span className='title text-pink-700'>e</span>
+                        </p>
+                    </div>
                 </Link>
-                <div className=''>
-                    <Link to='/' className='link-tag'>Home</Link>
-                    <Link to='statistics' className='link-tag'>Statistics</Link>
-                    <Link to='application' className='link-tag'>Application Job</Link>
-                    <Link to='blog' className='link-tag'>Blog</Link>
-                </div>
+                {/* nav items section activities */}
+                <ul className=' lg:flex items-center hidden space-x-8'>
+                    <li>
+                        <NavLink
+                            to='/'
+                            className={({ isActive }) => (isActive ? 'active' : 'default')}>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='statistics'
+                            className={({ isActive }) => (isActive ? 'active' : 'default')}>
+                            Statistics
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='application'
+                            className={({ isActive }) => (isActive ? 'active' : 'default')}>
+                            Application Job
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='blog'
+                            className={({ isActive }) => (isActive ? 'active' : 'default')}>
+                            Blog
+                        </NavLink>
+                    </li>
+                </ul>
+
+                {/* star button style */}
                 <div>
-                    <button>Star Applying</button>
+                    <button className='btn-star'>Star Applying</button>
                 </div>
             </div>
         </div>
